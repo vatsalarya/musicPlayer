74870e4debdf8b3051d58c18a3394fae324e81a8
Vatsal Arya <vatsalarya1@gmail.com>
2021-07-29 23:10:32 +0530

    Create home/main page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,104 @@
+import React, {useContext, useEffect, useState} from "react";
+import './css/Home.scss';
+import Navigation from "../fragment/Navigation";
+import {useSelector} from "react-redux";
+import {ThemeContext} from "../../api/Theme";
+import MobileTopNavigation from "../fragment/MobileTopNavigation";
+import SideBar from "../fragment/SideBar";
+import FooterMusicPlayer from "../fragment/FooterMusicPlayer";
+import BottomNavigationMobile from "../fragment/BottomNavigationMobile";
+import MusicCardContainer from "../fragment/MusicCardContainer";
+import AddMusic from "../fragment/AddMusic";
+import FooterSelectMusic from "../fragment/FooterSelectMusic";
+import CurrentPlayingLarge from "../fragment/CurrentPlayingLarge";
+import Playlist from "../fragment/Playlist";
+import {Skeleton} from "@material-ui/lab";
+
+
+function Home() {
+
+
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currMusic, setCurrMusic] = useState(null);
+    const [Page, setCurrPage] = useState(<MusicCardContainer/>);
+
+    let pathname = window.location.pathname;
+    useEffect(() => {
+        setCurrPage(getCurrPage(pathname))
+    }, [pathname]);
+
+    window.addEventListener("resize", handleResize);
+
+    function handleResize() {
+        setScreenSize(window.innerWidth);
+    }
+
+    useEffect(() => {
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    });
+
+    const useStyle = useContext(ThemeContext);
+    const {playing, bannerOpen} = useSelector(state => state.musicReducer);
+
+
+    useEffect(() => {
+        setCurrMusic(playing)
+    }, [playing])
+
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        setLoaded(true)
+    }, []);
+
+
+    return (
+        <div style={useStyle.component} className={"home-container"}>
+            {
+                !loaded ?
+                    <div className="Home-skeleton">
+                        <Skeleton animation={"wave"} variant={"rect"} height={"100vh"}/>
+                    </div>
+                    :
+                    <>
+                        {
+                            screenSize <= 970 ?
+                                <MobileTopNavigation/> :
+                                <Navigation/>
+                        }
+                        <section className={"home-music-container"}>
+                            <div className="sidebar-home">
+                                <SideBar/>
+                            </div>
+                            <div className="main-home">
+                                {
+                                    Page
+                                }
+                            </div>
+                        </section>
+                        {
+                            bannerOpen
+                            &&
+                            <section className="current-large-banner">
+                                <CurrentPlayingLarge/>
+                            </section>
+                        }
+                        <React.Fragment>
+                            {
+                                currMusic
+                                    ?
+                                    <FooterMusicPlayer music={currMusic}/>
+                                    :
+                                    <FooterSelectMusic/>
+                            }
+                            {
+                                screenSize <= 970 && <BottomNavigationMobile/>
+                            }
+                        </React.Fragment>
+                    </>
+            }
+        </div>
+    );
+}
+
+export default Home;
